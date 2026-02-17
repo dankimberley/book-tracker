31d08bc8c7462f3b368e1bd2fabef4e3dbae7d90
Dan Kimberley <danielkimberley@gmail.com>
2026-02-17 10:37:52 +0000

    make books request from front end

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,20 @@
 import './App.css'
 import { useEffect, useState } from 'react'
 
+type Book = {
+  id: number,
+  user_id: number,
+  title: string,
+  author: string,
+  date_finished: string,
+  completed: number,
+  created_at: string
+}
+
 function App() {
 
   const [message, setMessage] = useState<string>('')
+  const [books, setBooks] = useState<Book[]>([])
 
   useEffect(() => {
     fetch('http://localhost:3000')
@@ -12,11 +23,24 @@ function App() {
     .catch(error => console.error('Error:', error))
   })
 
+  useEffect(() => {
+    fetch('http://localhost:3000/api/books')
+    .then(response => response.json())
+    .then(data => setBooks(data))
+    .catch(error => console.error('Error:', error))
+  })
+
   return (
     <>
       <div>
         Book Tracker
         {message}
+        {books.map(book => (
+          <div key={book.id}>
+            <h3>{book.title}</h3>
+            <p>by {book.author}</p>
+          </div>
+        ))}
       </div>
     </>
   )
