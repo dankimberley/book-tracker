1dddebb9e4eb570f1056c9d67fcdbb55e19437d2
Dan Kimberley <danielkimberley@gmail.com>
2026-02-17 09:48:03 +0000

    front end connected to back end

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,22 @@
 import './App.css'
+import { useEffect, useState } from 'react'
 
 function App() {
+
+  const [message, setMessage] = useState<string>('')
+
+  useEffect(() => {
+    fetch('http://localhost:3000')
+    .then(response => response.json())
+    .then(data => setMessage(data.message))
+    .catch(error => console.error('Error:', error))
+  })
 
   return (
     <>
       <div>
         Book Tracker
+        {message}
       </div>
     </>
   )
