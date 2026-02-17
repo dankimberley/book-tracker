7092dd5b53d3164b4bbb57eab727d33f199e0d4c
Dan Kimberley <danielkimberley@gmail.com>
2026-02-17 10:33:32 +0000

    primitive book getting and posting

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,6 +1,8 @@
 import mysql from 'mysql2/promise'
 import dotenv from 'dotenv'
 
+dotenv.config()
+
 const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
 
 if (!DB_HOST || !DB_USER || !DB_PASSWORD || !DB_NAME) {
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,35 @@
-import express from 'express'
-import cors from 'cors'
-import pool from './db.js'
+import express from "express";
+import cors from "cors";
+import pool from "./db.js";
 
-const app = express()
+const app = express();
 const port = 3000;
 
-app.use(cors())
+app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.json({ message: 'Hello'})
-})
+app.get("/", (req, res) => {
+  res.json({ message: "Hello" });
+});
+
+app.get("/api/books", async (req, res) => {
+  const [books] = await pool.query("SELECT * FROM books");
+  res.json(books);
+});
+
+app.post("/api/books", async (req, res) => {
+try {
+    const { title, author, date_finished, completed } = req.body;
+    await pool.query(
+        "INSERT INTO books (user_id, title, author, date_finished, completed) VALUES (?,?,?,?,?)",
+        [1, title, author, date_finished, completed],
+    );
+    res.json({ success: true });
+} catch (error) {
+    console.error(error)
+    res.status(500).json({ error: "Failed to add book" });
+}
+});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
